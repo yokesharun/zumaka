468555039a3f103a883522431f575634de99fae0
ayokesh <ayokesh.cse@gmail.com>
2016-03-28 22:41:03 +0530

    layout changes

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout')
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
