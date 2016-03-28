5828a90aa7ed18abdd2a07b59d7231f41f9d2e89
ayokesh <ayokesh.cse@gmail.com>
2016-03-29 00:27:12 +0530

    express js mysql model added

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var admin = require('./../models/admin')
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'tv'
-});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-	connection.query('SELECT * from admins', function(err, rows, fields) {
-  if (err) throw err;
-
-    //test
-
-  res.render('index', { layout: 'layout', rows : rows});
-    
-
+router.get('/', function(req, res) {
+	admin.get(res);
 });
 
-});
 
 module.exports = router;
