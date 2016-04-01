b8be34ca3c7617deb3a0c7478de1cb7b9da66c8d
ayokesh <ayokesh.cse@gmail.com>
2016-04-02 00:38:11 +0530

    strucked with express validator

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var admin = require('./../models/admin')
-
+var admin = require('./../models/admin');
 
 /* GET home page. */
 router.get('/', function(req, res) {
 	admin.get(res);
 });
+
+router.get('/settings', function(req, res) {
+	res.render('settings');
+});
+
+router.post('/settings-process', function(req, res) {
+
+	req.assert("email_limit", "Enter a Email Limit.");
+
+	var errors = req.validationErrors();
+	  if (errors) {
+	  	res.send(errors);
+	    // res.redirect('back',{flash_errors:errors});
+	    return;
+	  } else {
+
+	  }
+
+	
+});
+
 
 //The 404 Route (ALWAYS Keep this as the last route)
 router.get('*', function(req, res){
