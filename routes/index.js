10fc09485e0b23dd01d8caed554092303794823f
ayokesh <ayokesh.cse@gmail.com>
2016-04-08 23:32:22 +0530

    emails adding initialised

--- routes/index.js
+++ routes/index.js
@@ -3,50 +3,18 @@ var router = express.Router();
 var settings = require('./../models/settings');
 var project = require('./../models/project');
 
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
 	settings.get(res);
 });
 
+
+// projects
+
 router.get('/add-project', function(req, res) {
 	res.render('add_project');
-});
-
-
-router.get('/list-projects', function(req, res) {
-	project.list_project(req,res);
-});
-
-router.get('/edit-project/:id', function(req, res) {
-	project.edit_project(req,res);
-});
-
-router.get('/delete-project/:id', function(req, res) {
-	project.delete_project(req,res);
-});
-
-
-router.get('/settings', function(req, res) {
-	settings.get_limit(res);
-});
-
-router.post('/settings-process', function(req, res) {
-
-	req.assert('email_limit','Invalid Email Limit').notEmpty()
-	   .isInt({ min: 0, max: 2000 }).withMessage('Email limit should be a number and less than 2000');
-    req.assert('email','Enter the Email').notEmpty();
-    req.assert('email', 'valid email required').isEmail();
-    req.assert('email_password','Enter the Email Password').notEmpty();
-
-	var errors = req.validationErrors();
-	  if (errors) {
-	  	// res.send(errors);
-	  	req.flash('flash_errors', errors)
-	    res.redirect('/settings');
-	    return;
-	  }else{
-		settings.update_limit(req,res);
-	  }
 });
 
 router.post('/add-project-process', function(req, res) {
@@ -79,6 +47,57 @@ router.post('/edit-project-process', function(req, res) {
 		project.edit_project_process(req,res);
 	  }
 });
+
+
+router.get('/list-projects', function(req, res) {
+	project.list_project(req,res);
+});
+
+router.get('/edit-project/:id', function(req, res) {
+	project.edit_project(req,res);
+});
+
+router.get('/delete-project/:id', function(req, res) {
+	project.delete_project(req,res);
+});
+
+
+
+// emails
+
+router.get('/add-email', function(req, res) {
+	res.render('add_email');
+});
+
+
+
+
+// settings
+
+router.get('/settings', function(req, res) {
+	settings.get_limit(res);
+});
+
+router.post('/settings-process', function(req, res) {
+
+	req.assert('email_limit','Invalid Email Limit').notEmpty()
+	   .isInt({ min: 0, max: 2000 }).withMessage('Email limit should be a number and less than 2000');
+    req.assert('email','Enter the Email').notEmpty();
+    req.assert('email', 'valid email required').isEmail();
+    req.assert('email_password','Enter the Email Password').notEmpty();
+
+	var errors = req.validationErrors();
+	  if (errors) {
+	  	// res.send(errors);
+	  	req.flash('flash_errors', errors)
+	    res.redirect('/settings');
+	    return;
+	  }else{
+		settings.update_limit(req,res);
+	  }
+});
+
+
 
 
 //The 404 Route (ALWAYS Keep this as the last route)
